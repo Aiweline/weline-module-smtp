3570e46730fb6db4137ecef01723bf7dcccadd45
Aiweline <32635766+Aiweline@users.noreply.github.com>
2025-02-25 20:30:15 +0800

    优化

--- Model/SmtpSendLog.php
+++ Model/SmtpSendLog.php
@@ -57,94 +57,94 @@ class SmtpSendLog extends \Weline\Framework\Database\Model
 //        $setup->dropTable();
         if (!$setup->tableExist()) {
             $setup->createTable()
-                ->addColumn(
-                    self::fields_ID,
-                    TableInterface::column_type_INTEGER,
-                    0,
-                    'primary key auto_increment',
-                    '发送者邮箱')
-                ->addColumn(
-                    self::fields_FROM,
-                    TableInterface::column_type_VARCHAR,
-                    255,
-                    'not null',
-                    '发送者邮箱')
-                ->addColumn(
-                    self::fields_SENDER_NAME,
-                    TableInterface::column_type_VARCHAR,
-                    30,
-                    'not null',
-                    '发送者昵称')
-                ->addColumn(
-                    self::fields_TO,
-                    TableInterface::column_type_MEDIU_TEXT,
-                    0,
-                    'not null',
-                    '接收者邮箱(一个或者多个)')
-                ->addColumn(
-                    self::fields_RPOXY,
-                    TableInterface::column_type_VARCHAR,
-                    255,
-                    'not null',
-                    '代理发送者')
-                ->addColumn(
-                    self::fields_REPLY_TO,
-                    TableInterface::column_type_MEDIU_TEXT,
-                    0,
-                    '',
-                    '回复（一个或者多个）')
-                ->addColumn(
-                    self::fields_SUBJECT,
-                    TableInterface::column_type_VARCHAR,
-                    255,
-                    'not null',
-                    '邮件标题')
-                ->addColumn(
-                    self::fields_CONTEXT,
-                    TableInterface::column_type_LONG_TEXT,
-                    0,
-                    'not null',
-                    '邮件内容')
-                ->addColumn(
-                    self::fields_ALT,
-                    TableInterface::column_type_MEDIU_TEXT,
-                    0,
-                    '',
-                    '邮件签名')
-                ->addColumn(
-                    self::fields_CC,
-                    TableInterface::column_type_MEDIU_TEXT,
-                    0,
-                    '',
-                    '邮件抄送（一个或者多个）')
-                ->addColumn(
-                    self::fields_BCC,
-                    TableInterface::column_type_MEDIU_TEXT,
-                    0,
-                    '',
-                    '隐蔽邮件抄送（一个或者多个）')
-                ->addColumn(
-                    self::fields_IS_HTML,
-                    TableInterface::column_type_SMALLINT,
-                    1,
-                    'default 1',
-                    '是否是html')
-                ->addColumn(
-                    self::fields_ATTACHMENT,
-                    TableInterface::column_type_MEDIU_TEXT,
-                    0,
-                    '',
-                    '附件（一个或者多个）')
-                ->addColumn(
-                    self::fields_MODULE,
-                    TableInterface::column_type_VARCHAR,
-                    128,
-                    'not null',
-                    '模组')
-                ->addIndex(TableInterface::index_type_FULLTEXT, 'FROM_EMAIL', self::fields_FROM)
-                ->addIndex(TableInterface::index_type_FULLTEXT, 'TO_EMAIL', self::fields_TO)
-                ->addIndex(TableInterface::index_type_FULLTEXT, 'SEND_MODULE', self::fields_MODULE)
-                ->create();
+                  ->addColumn(
+                      self::fields_ID,
+                      TableInterface::column_type_INTEGER,
+                      0,
+                      'primary key auto_increment',
+                      '发送者邮箱')
+                  ->addColumn(
+                      self::fields_FROM,
+                      TableInterface::column_type_VARCHAR,
+                      255,
+                      'not null',
+                      '发送者邮箱')
+                  ->addColumn(
+                      self::fields_SENDER_NAME,
+                      TableInterface::column_type_VARCHAR,
+                      30,
+                      'not null',
+                      '发送者昵称')
+                  ->addColumn(
+                      self::fields_TO,
+                      TableInterface::column_type_MEDIU_TEXT,
+                      0,
+                      'not null',
+                      '接收者邮箱(一个或者多个)')
+                  ->addColumn(
+                      self::fields_RPOXY,
+                      TableInterface::column_type_VARCHAR,
+                      255,
+                      'not null',
+                      '代理发送者')
+                  ->addColumn(
+                      self::fields_REPLY_TO,
+                      TableInterface::column_type_MEDIU_TEXT,
+                      0,
+                      '',
+                      '回复（一个或者多个）')
+                  ->addColumn(
+                      self::fields_SUBJECT,
+                      TableInterface::column_type_VARCHAR,
+                      255,
+                      'not null',
+                      '邮件标题')
+                  ->addColumn(
+                      self::fields_CONTEXT,
+                      TableInterface::column_type_LONG_TEXT,
+                      0,
+                      'not null',
+                      '邮件内容')
+                  ->addColumn(
+                      self::fields_ALT,
+                      TableInterface::column_type_MEDIU_TEXT,
+                      0,
+                      '',
+                      '邮件签名')
+                  ->addColumn(
+                      self::fields_CC,
+                      TableInterface::column_type_MEDIU_TEXT,
+                      0,
+                      '',
+                      '邮件抄送（一个或者多个）')
+                  ->addColumn(
+                      self::fields_BCC,
+                      TableInterface::column_type_MEDIU_TEXT,
+                      0,
+                      '',
+                      '隐蔽邮件抄送（一个或者多个）')
+                  ->addColumn(
+                      self::fields_IS_HTML,
+                      TableInterface::column_type_SMALLINT,
+                      1,
+                      'default 1',
+                      '是否是html')
+                  ->addColumn(
+                      self::fields_ATTACHMENT,
+                      TableInterface::column_type_MEDIU_TEXT,
+                      0,
+                      '',
+                      '附件（一个或者多个）')
+                  ->addColumn(
+                      self::fields_MODULE,
+                      TableInterface::column_type_VARCHAR,
+                      128,
+                      'not null',
+                      '模组')
+                  ->addIndex(TableInterface::index_type_FULLTEXT, 'FROM_EMAIL', self::fields_FROM)
+                  ->addIndex(TableInterface::index_type_FULLTEXT, 'TO_EMAIL', self::fields_TO)
+                  ->addIndex(TableInterface::index_type_FULLTEXT, 'SEND_MODULE', self::fields_MODULE)
+                  ->create();
         }
     }
 }
